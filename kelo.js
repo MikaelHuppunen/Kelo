c3cbd34574d4f29ce0094e48ae79e05ddd4c83b6
MikaelHuppunen <98363447+MikaelHuppunen@users.noreply.github.com>
2023-03-15 22:26:51 +0200

    Changed visual outlook of kelo page

--- kelo.js
+++ kelo.js
@@ -12,11 +12,12 @@ function readfile(filepath){
 
 data = JSON.parse(readfile("kelo.txt"));
 
-document.write("<font size='+2'>");
+document.write("<font size=6vh>");
 document.write('<div style="position: relative; width: 100%;">');
 for(let i = 0; i < data.length; i++){  
-  document.write('<div style="position: absolute; left: 0; top: '+ (i*25) + 'px;">');
-  document.write(data[i][0]);
+  document.write('<div style="position: absolute; left: 38vw; top: '+ (i*5) + 'vh;">');
+  document.write(i+1 + ".");
+  document.write('<span style="position: absolute; left: 3vw; top: 0;">'  + data[i][0] + '</span>');
   document.write('<span style="position: absolute; left: 15vw; top: 0;">' + Math.round(data[i][1]*10)/10 + '</span>');
   document.write('</div>');
 }
